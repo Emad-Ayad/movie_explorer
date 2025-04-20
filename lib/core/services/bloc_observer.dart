ac1eee5c75a3272c4568d3e7a56b4772448025de
Emad-Ayad <emadayad228@gmail.com>
2025-04-20 23:47:54 +0200

    add bloc observer

---
+++ lib/core/services/bloc_observer.dart
@@ -0,0 +1,14 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+
+class BlocObserverService extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    if (kDebugMode) {
+      log('${bloc.runtimeType} $change');
+    }
+  }
+}
